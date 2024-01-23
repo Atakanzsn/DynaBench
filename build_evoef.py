fef31d2556757c57751358aea109bbe5ce279752
Atakanzsn <atakanzsn@icloud.com>
2024-01-23 13:53:59 +0300

    bug fixes

--- build_evoef.py
+++ build_evoef.py
@@ -4,41 +4,38 @@ from sys import platform
 import shutil
 
 def build_evoef():
+    
+    cp = os.getcwd()
+    os.chdir('..')
 
-    os.mkdir('perm')
-    shutil.move('DynaBench', 'perm')
+    #os.mkdir('perm')
+    #shutil.move('DynaBench', 'perm')
 
     import DynaBench
 
-    cp = os.getcwd()
     path = DynaBench.__file__[:-12]
 
     os.chdir(path)
 
-    try:
 
-        if platform == "linux" or platform == "linux2":
-            os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
-            os.chdir("./EvoEF/")
-            os.system("g++ -O3 --fast-math -o EvoEF src/*.cpp")
-            os.chdir("../")
+    if platform == "linux" or platform == "linux2":
+        os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
+        os.chdir("./EvoEF/")
+        os.system("g++ -O3 --fast-math -o EvoEF src/*.cpp")
+        os.chdir("../")
 
-        elif platform == "darwin":
-            os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
-            os.chdir("./EvoEF/")
-            os.system("g++ -O3 -ffast-math -o EvoEF src/*.cpp")
-            os.chdir("../")
+    elif platform == "darwin":
+        os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
+        os.chdir("./EvoEF/")
+        os.system("g++ -O3 -ffast-math -o EvoEF src/*.cpp")
+        os.chdir("../")
 
-        elif platform == 'win32':
-            os.system('wsl git clone https://github.com/tommyhuangthu/EvoEF.git')
-            os.chdir("EvoEF/")
-            os.system("wsl g++ -O3 --fast-math -o EvoEF src/*.cpp")
-            os.chdir("../")
-
-        os.chdir(cp)
-    finally:
-        shutil.move('./perm/DynaBench', cp)
-        os.rmdir('perm')
+    elif platform == 'win32':
+        os.system('wsl git clone https://github.com/tommyhuangthu/EvoEF.git')
+        os.chdir("EvoEF/")
+        os.system("wsl g++ -O3 --fast-math -o EvoEF src/*.cpp")
+        os.chdir("../")
+    os.chdir(cp)
 
 build_evoef()
 
