309ba8a1c341f1a225f99f50ee3614d3b51b6647
Atakanzsn <atakanzsn@icloud.com>
2024-01-25 14:06:57 +0300

    evoef_bug_fixes

--- build_evoef.py
+++ build_evoef.py
@@ -3,10 +3,13 @@ import os
 from sys import platform
 import shutil
 
+import argparse
+
+parser = argparse.ArgumentParser()
+
 def build_evoef():
     
     cp = os.getcwd()
-    #os.chdir('..\..')
 
     os.mkdir('perm')
     shutil.move('DynaBench', 'perm')
@@ -15,27 +18,39 @@ def build_evoef():
 
     path = DynaBench.__file__[:-12]
 
-    os.chdir(path)
+
+    if platform == "win32":
+        def install_evoef(path):
+            
+            dynabench_destination = DynaBench.__file__[:-12]
+            print(dynabench_destination)
+            shutil.move(path, dynabench_destination)
 
 
-    if platform == "linux" or platform == "linux2":
-        os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
-        os.chdir("./EvoEF/")
-        os.system("g++ -O3 --fast-math -o EvoEF src/*.cpp")
-        os.chdir("../")
+        parser.add_argument("--evoef_path", type=str)
+        args = parser.parse_args()
 
-    elif platform == "darwin":
-        os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
-        os.chdir("./EvoEF/")
-        os.system("g++ -O3 -ffast-math -o EvoEF src/*.cpp")
-        os.chdir("../")
+        install_evoef(args.evoef_path)
+        
+    else:
 
-    elif platform == 'win32':
-        os.system('wsl git clone https://github.com/tommyhuangthu/EvoEF.git')
-        os.chdir("EvoEF/")
-        os.system("wsl g++ -O3 --fast-math -o EvoEF src/*.cpp")
-        os.chdir("../")
-    os.chdir(cp)
+        os.chdir(path)
+
+
+        if platform == "linux" or platform == "linux2":
+            os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
+            os.chdir("./EvoEF/")
+            os.system("g++ -O3 --fast-math -o EvoEF src/*.cpp")
+            os.chdir("../")
+
+        elif platform == "darwin":
+            os.system('git clone https://github.com/tommyhuangthu/EvoEF.git')
+            os.chdir("./EvoEF/")
+            os.system("g++ -O3 -ffast-math -o EvoEF src/*.cpp")
+            os.chdir("../")
+
+        os.chdir(cp)
+
     shutil.move('./perm/DynaBench', cp)
     os.rmdir('perm')
 
